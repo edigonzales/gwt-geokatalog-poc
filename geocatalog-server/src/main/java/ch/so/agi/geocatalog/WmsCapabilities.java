9f2e6d3cce69920b905e726b75de086ca02d8176
Stefan Ziegler <edi.gonzales@gmail.com>
2021-01-23 22:31:29 +0100

    clean code

--- geocatalog-server/src/main/java/ch/so/agi/geocatalog/WmsCapabilities.java
+++ geocatalog-server/src/main/java/ch/so/agi/geocatalog/WmsCapabilities.java
@@ -13,12 +13,14 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+@Service
 public class WmsCapabilities {
     String url;
     File file;
